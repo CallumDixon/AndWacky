76ddb767415eab1493c42ad314d48cbb73c77506
Callum Dixon <callum.dixon@and.digital>
2022-06-14 16:51:51 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [state, setState] = useState(" ")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <h1>{state}</h1>
+    <form>
+      <input type="text" onChange={(data) => {
+        console.log(data.target.value)
+        setState(data.target.value)
+      }} >
+
+      </input>
+    </form>
     </div>
-  );
+
+  )
+    
 }
 
 export default App;
